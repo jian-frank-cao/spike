8074d0cc04e0cd7b26fcf1de02c6c7be5dfc9eeb
Jian Cao <jccit@caltech.edu>
2021-01-20 22:28:14 -0800

    Update __init__.py

--- spike/__init__.py
+++ spike/__init__.py
@@ -1,3 +1,4 @@
 from spike.ConnectSFTP import ConnectSFTP
 from spike.ConnectGoogleDrive import ConnectGoogleDrive
+from spike.ConnectGoogleCloudStorage import ConnectGoogleCloudStorage
 from spike.my_sum import my_sum
