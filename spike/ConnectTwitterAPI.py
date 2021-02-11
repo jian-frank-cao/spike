6e606d1838b156f0f258903ad74ded46b85e5a40
Jian Cao <jccit@caltech.edu>
2021-02-11 14:41:21 -0800

    Update ConnectTwitterAPI.py

--- spike/ConnectTwitterAPI.py
+++ spike/ConnectTwitterAPI.py
@@ -13,6 +13,7 @@ import time
 import json
 import requests
 import uuid
+import multiprocessing
 from datetime import datetime, timedelta
 from requests import HTTPError, ConnectionError
 from JianTwitterAPI import JianTwitterAPI, TwitterConnectionError, TwitterRequestError
@@ -35,7 +36,6 @@ class ConnectTwitterAPI:
         self.consumer_secret = consumer_secret
         self.access_token_key = access_token_key
         self.access_token_secret = access_token_secret
-        self.tweets = []
 
 
     def GetBearerToken(self, key, secret):  # might not be necessary if use TwitterAPI
@@ -53,165 +53,195 @@ class ConnectTwitterAPI:
 
 
     def _get_ready(self):
-        self.twitter_api = None
-        self.bearer_token = None
-        
-        if 'stream_v1' in self.api_type:
+        # check input
+        if not any([x == self.api_type for 
+                x in ['stream_v1', 'rest_v1', 'lab-covid19']]): # add stream_v2, rest_v2
+            raise Exception('API ' + self.api_type + 
+                            ' is not currently supported')
+        if not any([x == self.outlet_type for 
+                x in ['local', 'local_count']]): # add pubsub, kinesis, oracle stream
+            raise Exception('OUTLET ' + self.outlet_type + 
+                            ' is not currently supported')
+
+        # authorization
+        if self.api_type == 'stream_v1':
             self.twitter_api = JianTwitterAPI(self.consumer_key,
                                          self.consumer_secret,
                                          self.access_token_key,
                                          self.access_token_secret)
-            print('Stream API v1.1 is ready.')
-            
-        if 'rest_v1' in self.api_type:
+            print('oAuth1 is ready.')
+        if any(x == self.api_type for x in ['rest_v1', 'lab_covid19']):
             self.twitter_api = JianTwitterAPI(self.consumer_key,
                                          self.consumer_secret,
                                          auth_type='oAuth2')
-            print('REST API v1.1 is ready.')
-            
-        if 'lab_covid19' in self.api_type:   # find a way to combine this
-            self.twitter_api = JianTwitterAPI(self.consumer_key,
-                                         self.consumer_secret,
-                                         auth_type='oAuth2')
-            print('Lab API COVID19 is ready.')
-            
-        if any(x in self.api_type for x in ['stream_v2', 'rest_v2']): # modify this to use TwitterAPI
+            print('oAuth2 is ready.')
+        if any(x == self.api_type for x in ['stream_v2', 'rest_v2']): # modify this to use TwitterAPI
             self.bearer_token = self.GetBearerToken(self.consumer_key,
                                                     self.consumer_secret)
         
-        if 'local' in self.outlet_type:
-            if 'count' in self.outlet_type:
-                self.tweets_per_file = 15000
-                self.tweet_count = 0
-                if 'tweets_per_file' in self.input_dict:
-                    self.tweets_per_file = self.input_dict['tweets_per_file']
-            else:
-                self.minutes_per_file = timedelta(minutes = 15)
-                self.file_timer = None
-                if 'minutes_per_file' in self.input_dict:
-                    self.minutes_per_file = timedelta(
-                        minutes = float(self.input_dict['minutes_per_file']))
-            
+        # prepare requests
+        if 'stream_v1' in self.api_type:
+            if any(x not in self.input_dict for x in
+               ['keywords']):
+                raise ValueError('KEYWORDS is needed.')
+            self.resource = 'statuses/filter'
+            self.params = {'track': self.input_dict['keywords']} # add more rules in the params as needed
+        
+        if 'rest_v1' in self.api_type:
+            if any(x not in self.input_dict for x in
+                   ['keywords', 'max_id', 'since_id']):
+                raise ValueError('KEYWORDS, MAX_ID, and SINCE_ID are needed.')
+            keywords = '(' + ') OR ('.join(self.input_dict['keywords']) + ')'
+            self.resource = 'search/tweets'
+            self.params = {'q': keywords,
+                     'max_id': self.input_dict['max_id'],
+                     'since_id': self.input_dict['since_id'],
+                     'count': 100,
+                     'tweet_mode': 'extended'}
+            if 'tweets_per_qry' in self.input_dict:
+                self.params['count'] = self.input_dict['tweets_per_qry']
+            self.tweet_downloaded = 0
+        
+        if 'lab_covid19' in self.api_type:
+            if any(x not in self.input_dict for x in
+                   ['partition']):
+                raise ValueError('PARTITION is needed.')
+            self.params = {'partition': self.input_dict['partition']}
+            self.resource = 'labs/1/tweets/stream/covid19'
+        
+        # prepare outlet
+        if not self.pipe_in or not self.pip_out:
+            self.pipe_in, self.pip_out = multiprocessing.Pipe()
+        if ('local' in self.outlet_type and 
+            not self.tweets):
+            self.tweets = []
+        if self.outlet_type == 'local_count':
+            self.tweets_per_file = 15000
+            if 'tweets_per_file' in self.input_dict:
+                self.tweets_per_file = self.input_dict['tweets_per_file']
+        if self.outlet_type == 'local':
+            self.minutes_per_file = timedelta(minutes = 15)
+            if 'minutes_per_file' in self.input_dict:
+                self.minutes_per_file = timedelta(
+                    minutes = float(self.input_dict['minutes_per_file']))
+        if any(x not in self.input_dict for x in
+               ['file_prefix', 'download_path']):
+            raise ValueError('FILE_PREFIX, DOWNLOAD_PATH are needed.')
 
 
     def _request_stream_v1(self):
-        if any(x not in self.input_dict for x in
-               ['keywords']):
-            raise ValueError('KEYWORDS is needed.')
-        
-        # prepare params 
-        params = {'track': self.input_dict['keywords']} # add more rules in the params as needed
-        
-        # make request
-        response = self.twitter_api.request('statuses/filter', params)
-        return(response)
+        self.response = self.twitter_api.request(self.resource,
+                                                 self.params)
+        if 'stream_v1' in self.api_type:
+            print('Connected to Stream API v1.1.')
+            print('First 10 keywords: ' +
+                  ', '.join(self.input_dict['keywords'][:10]) + '.')
+        else:
+            print(('Connected to Lab API COVID19 partition ' +
+                   str(self.input_dict['partition'])))
+        print('Collecting tweets...')
+        for tweet in self.response:
+            if 'text' in tweet:
+                self.pipe_in.send(tweet)
+            elif 'disconnect' in tweet:
+                event = tweet['disconnect']
+                if event['code'] in [2,5,6,7]:
+                    raise Exception(event['reason']) # something needs to be fixed before re-connecting
+                else:
+                    print(('Disconnect Code: ' + event['code'] +
+                          '. Reason: ' + event['reason']))
+                    return(True) # temporary interruption, re-try request
+        return(True) # stream stopped with no reason, re-try request
 
 
     def _request_rest_v1(self):
-        if any(x not in self.input_dict for x in
-               ['keywords', 'max_id', 'since_id']):
-            raise ValueError('KEYWORDS, MAX_ID, and SINCE_ID are needed.')
-        
-        # prepare params
-        keywords = '(' + ') OR ('.join(self.input_dict['keywords']) + ')'
-        params = {'q': keywords,
-                 'max_id': self.input_dict['max_id'],
-                 'since_id': self.input_dict['since_id'],
-                 'count': 100,
-                 'tweet_mode': 'extended'}
-        if 'tweets_per_qry' in self.input_dict:
-            params['count'] = self.input_dict['tweets_per_qry']
-        
-        # make request
-        response = self.twitter_api.request('search/tweets', params)
-        return(response)
-
-
-    def _request_lab_covid19(self):
-        if any(x not in self.input_dict for x in
-               ['partition']):
-            raise ValueError('PARTITION is needed.')
-        
-        # prepare params
-        params = {'partition': self.input_dict['partition']}
-        
-        # make request
-        response = self.twitter_api.request('labs/1/tweets/stream/covid19',
-                                            params)
-        return(response)
+        self.response = self.twitter_api.request(self.resource,
+                                                 self.params)
+        if (not self.response.json() or 
+            'statuses' not in self.response.json()):
+            raise TypeError ('"statuses" not in response.json().')
+        tweets = self.response.json()['statuses']
+        n_tweets = len(tweets)
+        for tweet in tweets:
+            self.pipe_in.send(tweet)
+        if n_tweets == 0:
+            print('No more tweets found.')
+            self.pipe_in.send("FINISHED")
+            return(False)
+        self.tweet_downloaded += n_tweets
+        print('Downloaded {} tweets.'.format(self.tweet_downloaded))
+        self.input_dict['max_id'] = tweet['id'] - 1
+        return(True)
 
 
     def _collect_tweets(self):
-        if 'rest_v1' in self.api_type:
-            n_tweets = 0
-            response = self._request_rest_v1()
-            self.request_headers = response.headers
-            if not response.json() or 'statuses' not in response.json():
-                raise TypeError ('"statuses" not in response.json().')
-            tweets = response.json()['statuses']
-            n_tweets = len(tweets)
-            for tweet in tweets:
-                self._data_outlet(tweet)
-            if n_tweets == 0:
-                print('No more tweets found.')
-                self._data_outlet(None)
+        last_error = None
+        go = True
+        while go:
+            retry = False
+            try:
+                if any(x in self.api_type for 
+                       x in ['stream_v1', 'lab_covid19']):
+                    go = self._request_stream_v1()
+                if 'rest_v1' in self.api_type:
+                    go = self._request_rest_v1()
+            except IOError as ioe:
+                print('[Caught IOError]\n' + str(ioe))
+                retry = True
+            except HTTPError as he:
+                print('[Caught HTTPError]\n' + str(he))
+                retry = True
+            except ConnectionError as ce:
+                print('[Caught ConnectionError]\n' + str(ce))
+                retry = True
+            except TypeError as te:
+                print('[Caught TimeoutError]\n' + str(te))
+                retry = True
+            except TwitterConnectionError as tce:
+                print('[Caught TwitterConnectionError]\n' + str(tce))
+                retry = True
+            except TwitterRequestError as tre:
+                print('[Caught TwitterRequestError]\n' + str(tre))
+                retry = True
+            # retry strategy 
+            if not retry:
+                if 'rest_v1' in self.api_type:
+                    time.sleep(2.1)
+                continue
+            print(self.request.headers)
+            if not last_error:
+                last_error = datetime.now()
+                error_count = 0
+            if datetime.now() - last_error > timedelta(seconds = 900):
+                error_count = 0
+            wait = min(0.25 * 2**error_count, 30)
+            last_error = datetime.now()
+            error_count += 1
+            print('Wait {} seconds before retrying...'.format(wait))
+            time.sleep(wait)
+
+            
+    def _save_locally(self):
+        if self.outlet_type == 'local' and not self.file_timer:
+            self.file_timer = datetime.now() + self.minutes_per_file
+        print('Start saving tweets into local TXT files...')
+        while True:
+            tweet = self.pipe_out.recv()
+            if tweet == "FINISHED":
                 return(False)
-            self.tweet_count += n_tweets
-            print('Downloaded {} tweets.'.format(self.tweet_count))
-            self.input_dict['max_id'] = tweet['id'] - 1
-        
-        if any(x in self.api_type for x in ['stream_v1', 'lab_covid19']):
-            if 'stream_v1' in self.api_type:
-                response = self._request_stream_v1()
-                print('Connected to Stream API v1.1.')
-                print('First 10 keywords: ' +
-                      ', '.join(self.input_dict['keywords'][:10]) + '.')
-            else:
-                response = self._request_lab_covid19()
-                print(('Connected to Lab API COVID19 partition ' +
-                       str(self.input_dict['partition'])))
-            #if response.status_code != 200:       # this has been down in TwitterAPI
-            #    print(response.headers)
-            #    raise ConnectionError(response.text)
-            self.twitter_headers = response.headers
-            print('Collecting tweets...')
-            for tweet in response:
-                if 'text' in tweet:
-                    self._data_outlet(tweet)
-                elif 'disconnect' in tweet:
-                    event = tweet['disconnect']
-                    if event['code'] in [2,5,6,7]:
-                        raise Exception(event['reason']) # something needs to be fixed before re-connecting
-                    else:
-                        print(('Disconnect Code: ' + event['code'] +
-                              '. Reason: ' + event['reason']))
-                        return(True) # temporary interruption, re-try request
-        return(True)  # for rest api
-
-
-    def _data_outlet(self, tweet):
-        if 'local' in self.outlet_type:
-            if any(x not in self.input_dict for x in
-                   ['file_prefix', 'download_path']):
-                raise ValueError('FILE_PREFIX, DOWNLOAD_PATH are needed.')
-            # append tweet
-            if tweet:
-                self.tweets.append(tweet)
+            self.tweets.append(tweet)
+            self.tweet_count += 1
             # determine if the file is ready
             file_is_ready = False
             if 'count' in self.outlet_type:
-                file_is_ready = len(self.tweets) >= self.tweets_per_file
+                file_is_ready = self.tweet_count >= self.tweets_per_file
             else:
-                if not isinstance(self.file_timer, datetime):
-                    self.file_timer = datetime.now()
-                file_is_ready = (datetime.now() - self.file_timer >=
-                                 self.minutes_per_file)
-                if file_is_ready:
-                    self.file_timer = datetime.now()
+                file_is_ready = datetime.now() >= self.file_timer
             # file is not ready, continue adding tweets
-            if not file_is_ready and tweet:
-                return(None)
+            if not file_is_ready:
+                continue
             # save file
+            self.file_timer = datetime.now() + self.minutes_per_file
             tweet_time = self.tweets[-1]['created_at']
             time_format = '%a %b %d %H:%M:%S %z %Y'
             if 'v2' in self.api_type:
@@ -240,9 +270,28 @@ class ConnectTwitterAPI:
                           str(tweet['warning']['percent_full']) + '% full.')
             # clean self.tweets
             self.tweets = []
+            self.tweet_count = 0
+        return(True) # stopped with no reason, re-trying
 
 
-    def Start(self, input_dict,
+    def _tweet_outlet(self):
+        go = True
+        while go:
+            retry = False
+            try:
+                if 'local' in self.outlet_type:
+                    go = self._save_locally()
+            except: # find errors that may occur
+                retry = True
+                pass
+            # retry strategy   # find better retry strategy
+            if not retry:
+                continue
+            print('Tweet outlet failed, resetting...')
+            time.sleep(0.25)
+
+
+    def StartMonitor(self, input_dict,
               api_type = 'stream_v1',
               outlet_type = 'local'):
         """Start the monitor
@@ -267,46 +316,21 @@ class ConnectTwitterAPI:
         self._get_ready()
         
         # start monitor
-        last_error = None
-        go = True
-        while go:
-            retry = False
-            try:
-                go = self._collect_tweets()
-            except IOError as ioe:
-                print('[Caught IOError]\n' + str(ioe))
-                retry = True
-            except HTTPError as he:
-                print('[Caught HTTPError]\n' + str(he))
-                retry = True
-            except ConnectionError as ce:
-                print('[Caught ConnectionError]\n' + str(ce))
-                retry = True
-            except TypeError as te:
-                print('[Caught TimeoutError]\n' + str(te))
-                retry = True
-            except TwitterConnectionError as tce:
-                print('[Caught TwitterConnectionError]\n' + str(tce))
-                retry = True
-            except TwitterRequestError as tre:
-                print('[Caught TwitterRequestError]\n' + str(tre))
-                retry = True
-            # retry strategy                    # need to re-write this
-            if not retry:
-                if 'rest_v1' in self.api_type:
-                    time.sleep(2.1)
-                continue
-            print(self.twitter_headers)
-            if not isinstance(last_error, datetime):
-                last_error = datetime.now()
-                error_count = 0
-            if datetime.now() - last_error > timedelta(seconds = 900):
-                error_count = 0
-            wait = min(0.25 * 2**error_count, 30)
-            last_error = datetime.now()
-            error_count += 1
-            print('Wait {} seconds before retrying...'.format(wait))
-            time.sleep(wait)
+        tweets_in = multiprocessing.Process(target = self._collect_tweets,
+                                            args=())
+        tweets_out = multiprocessing.Process(target = self._tweet_outlet,
+                                            args=())
+        tweets_in.start()
+        tweets_out.start()
+        
+    	# when processes finish
+        tweets_in.join()
+        tweets_out.join()
+        self.pipe_in.close()
+        self.pipe_out.close()
+        
+        
+
             
 
 
